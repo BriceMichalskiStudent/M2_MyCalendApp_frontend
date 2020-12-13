c14ea199bfdc8ed69516a74537909a22111c29ca
Alban <pro.pierson.alban@gmail.com>
2020-12-13 12:51:07 +0100

    Minimal home

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,32 @@
 <template>
-  <div class="col-md-8 offset-md-2">
-    <h2>Wow ! Nice slider !</h2>
-    <EventCarousel :events="events" />
-    <br>
-    <br>
-    <br>
-    <h2>OMG ! An other one !</h2>
-    <EventCarousel />
-  </div>
+  <section class="col-md-10 offset-md-1 row">
+    <div class="col-md-8 home-content">
+      <h1>Trouvez et <br>partagez les évènements <br>qui vous interesse !</h1>
+      <Button anchor="Découvrir !" link="/events" custom="primary" />
+      <p class="col-md-8">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores blanditiis commodi dicta doloremque
+        ducimus eaque eius harum inventore, iure nostrum quam quidem repudiandae, sed, sit unde ut velit vitae voluptatum.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores blanditiis commodi dicta doloremque
+        ducimus eaque eius harum inventore, iure nostrum quam quidem repudiandae, sed, sit unde ut velit vitae voluptatum.
+        <br>
+        <br>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores blanditiis commodi dicta doloremque
+        ducimus eaque eius harum inventore, iure nostrum quam quidem repudiandae, sed, sit unde ut velit vitae voluptatum.
+      </p>
+    </div>
+    <img class="col-md-4" src="/img/mobie-home.png">
+    <div class="col-md-10 home-discover">
+      <h2>Futurs évènements !</h2>
+      <Button link="/" anchor="Voir tous" custom="primary" />
+      <EventCarousel :events="events" />
+    </div>
+  </section>
 </template>
 
 <script>
-import EventCarousel from '~/components/EventCarousel'
-
+import Button from '~/components/Button'
 export default {
-  components: { EventCarousel },
+  components: { Button },
   transition: 'opacity',
   data () {
     return {
@@ -131,24 +143,69 @@ export default {
 }
 </script>
 
-<style scoped>
-.center-here {
-  min-height: calc(100vh - 100px);
+<style scoped lang="scss">
+.home-content{
   display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+  flex-direction: column;
+  a{
+    align-self: center;
+    width: 300px;
+    margin: 30px auto;
+  }
+  p{
+    margin: 40px 0;
+    @media only screen and (max-width: map-get($grid-breakpoints, 'md')) {
+      margin: 10px 0;
+    }
+  }
 }
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+h1{
+  font-size: 90px;
+  line-height: 100px;
+  padding: 60px 20px;
+  @media only screen and (max-width: map-get($grid-breakpoints, 'md')) {
+    font-size: 50px;
+    line-height: 60px;
+    padding: 40px 10px;
+    br{
+      display: none;
+    }
+  }
 }
-
-.links {
-  padding-top: 15px;
+img{
+  padding: 40px;
+  object-fit: scale-down;
+  @media only screen and (max-width: map-get($grid-breakpoints, 'md')) {
+    display: none;
+  }
+}
+.home-discover{
+  margin-top: 100px;
+  @media only screen and (max-width: map-get($grid-breakpoints, 'md')) {
+    margin-top: 0;
+  }
+  h2{
+    display: inline-block;
+    padding: 30px 0;
+    width: 73%;
+    @media only screen and (max-width: map-get($grid-breakpoints, 'sm')) {
+      width: 60%;
+    }
+  }
+  a{
+    display: inline-block;
+    width: 27%;
+    text-align: center;
+    max-width: 200px;
+    float: right;
+    top: 20px;
+    @media only screen and (max-width: map-get($grid-breakpoints, 'sm')) {
+      padding: 10px 0;
+      font-size: 17px;
+      line-height: 27px;
+      width: 38%;
+      top:25px
+    }
+  }
 }
 </style>
