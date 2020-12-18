7320036b9411f7dfc77dd6fb398262a4e092f095
Alban <pro.pierson.alban@gmail.com>
2020-12-18 12:06:16 +0100

    Remove notification

--- middleware/connected.js
+++ middleware/connected.js
@@ -1,9 +1,5 @@
 export default function ({ store, redirect }) {
   if (store.state.auth.loggedIn !== true) {
-    store.commit('sendNotification', {
-      status: 'info',
-      message: 'Vous devez etre connecter pour acceder a cette page !'
-    })
     return redirect('/login')
   } else {
     for (let i = 0; i < store.state.auth.user.Roles.length; i++) {
